9ca837e19927389d0d0b9ad9950c0b76f563685a
Spencerreid559 <70726571+Spencerreids@users.noreply.github.com>
2024-10-05 13:35:17 -0600

    Updated API.js

--- api.js
+++ api.js
@@ -6,19 +6,18 @@ const supaAnonKey = 'Secret';
 const supabase = supa.createClient(supaUrl, supaAnonKey);
 
 // Client request + profiling
-app.get('/api/requests'), async (req,res) => {
-    const {data,error} = await supabase
-    .from('Request')
-    .select(`Client(name, talkativity, formality, friendliness, extroversion), 
-        time_required, additional`);
-    return data;
-}
+app.get('/api/requests', async (req,res) => {
+    const {data, error} = await supabase
+    .from('request')
+    .select(`client(name, talkativity, formality, friendliness, extroversion), time_required, additional`);
+    res.send(data);
+})
 
 // Client request + profiling
-app.get('/api/mentor/:name'), async (req,res) => {
+app.get('/api/mentor/:name', async (req,res) => {
     const {data,error} = await supabase
     .from('mentor')
-    .select(`name, available, talkativity, formality, friendliness, extroversion`)
-    .ilike('name', req.params.ref.toLowerCase());
-    return data;
-}
+    .select()
+    .ilike('name', req.params.name.toLowerCase());
+    res.send(data);
+})
