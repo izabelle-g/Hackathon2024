fcd1495d7851e79627e299ecbce09970e22d514b
Spencerreid559 <70726571+Spencerreids@users.noreply.github.com>
2024-10-05 21:33:35 -0600

    bugfix

--- mruhacks-react-app/src/components/Home.jsx
+++ mruhacks-react-app/src/components/Home.jsx
@@ -1,7 +1,13 @@
+import Sidebar from './Sidebar'
+import SessionInput from './SessionInput'
+import './Home.css'
+
 const Home = () => {
 	return(
 		<div>
 			<h2>Home</h2>
+			<Sidebar></Sidebar>
+			<SessionInput></SessionInput>
 		</div>
 	)
 };
